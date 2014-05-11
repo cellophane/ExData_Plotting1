61502091aca771896f6048554b33b86bf41560f2
cellophane <nikiswarthout@niki-swarthouts-computer.local>
2014-05-11 18:08:40 -0400

    fix assignment

--- plot1.R
+++ plot1.R
@@ -4,7 +4,7 @@ data <- fread(fileName,header=TRUE, na.strings="?",sep=";",colClasses="character
 data <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
 data$Global_active_power <- as.numeric(data$Global_active_power)
 png(filename = "plot1.png")
-plot(strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S"),data$Global_active_power,
-     ylab = "Global Active Power (kilowatts)", 
-                  main = "")
+dateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+attach(data)
+hist(Global_active_power,col='red', main= "Global Active Power", xlab = 'Global Active Power (kilowatts)')
 dev.off()
--- plot4.R
+++ plot4.R
@@ -6,13 +6,17 @@ data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
 data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
 data$Sub_metering_3 <- as.numeric(data$Sub_metering_3)
 dateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
-png("plot3.png")
+png("plot4.png")
 with(data,{
+     par(mfrow=c(2,2))
+     plot(dateTime,Global_active_power,t='l')
+     plot(dateTime,Voltage,t='l')
      plot(dateTime,Sub_metering_1,t='l',
           ylab="Energy sub metering", xlab="")
      lines(dateTime,Sub_metering_2,col='red')
      lines(dateTime,Sub_metering_3,col='blue')
      legend("topright",pch=1,col=c("black","red","blue"),
             legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+     plot(dateTime,Global_reactive_power,t='l')
 })
 dev.off()
